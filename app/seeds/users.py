1290b34db6fba080a28227487e0f499717456862
Mineh <minehgharabegi@gmail.com>
2022-08-03 10:06:34 -0700

    user seed data

--- app/seeds/users.py
+++ app/seeds/users.py
@@ -4,14 +4,14 @@ from app.models import db, User
 # Adds a demo user, you can add other users here if you want
 def seed_users():
     demo = User(
-        username='Demo', email='demo@aa.io', password='password')
-    marnie = User(
-        username='marnie', email='marnie@aa.io', password='password')
+        email='demo@aa.io', full_name='Demo User', username='Demo', bio="Hi! I'm Demo User. Please feel free to use my account to browse Picstagram and all its features! :)", profile_photo='https://mypicstagrambucket.s3.us-west-1.amazonaws.com/rose.jpeg', password='password')
+    cinnamon = User(
+        email='cinnamon@gmail.com', full_name='Cinnamon Gharabegi', username='Cinnamon', bio="Hi, I'm Cinnamon 🐶. You can call me Cinny.", profile_photo='https://mypicstagrambucket.s3.us-west-1.amazonaws.com/67937574350__61C71683-671A-4EEE-9D43-B56C7C999635.jpeg', password='password')
     bobbie = User(
-        username='bobbie', email='bobbie@aa.io', password='password')
+        email='bobbie@aa.io', full_name='Billy Bob', username='bobbie', bio='Photographer 📷 Follow me if you like beautiful landscape pictures!', profile_photo='https://mypicstagrambucket.s3.us-west-1.amazonaws.com/bobbie.jpeg', password='password')
 
     db.session.add(demo)
-    db.session.add(marnie)
+    db.session.add(cinnamon)
     db.session.add(bobbie)
 
     db.session.commit()

98c5acff70b5e22b363039c3fac61c7edb9d787e
Mineh <minehgharabegi@gmail.com>
2022-08-02 23:53:56 -0700

    AWS set up + tables/relationships done

---
+++ app/models/post.py
@@ -0,0 +1,33 @@
+from .db import db
+from .like import likes
+from sqlalchemy import DateTime
+from sqlalchemy.sql import func
+
+class Post(db.Model):
+    __tablename__ = 'posts'
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    picture = db.Column(db.String(255), nullable=False)
+    caption = db.Column(db.String(500))
+    created_at = db.Column(DateTime(timezone=True), server_default=func.now())
+    updated_at = db.Column(DateTime(timezone=True), onupdate=func.now())
+
+    user = db.relationship("User", back_populates="posts")
+    comments = db.relationship("Comment", back_populates="post")
+
+    post_likes = db.relationship("User",
+            secondary=likes,
+            back_populates="user_likes",
+            cascade="all, delete"
+    )
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'user_id': self.user_id,
+            'picture': self.picture,
+            'caption': self.caption,
+            'created_at': self.created_at,
+            'updated_at': self.updated_at
+        }
--- app/models/user.py
+++ app/models/user.py
@@ -1,4 +1,8 @@
 from .db import db
+from .like import likes
+from .follow import follows
+from sqlalchemy.sql import func
+from sqlalchemy import DateTime
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 
@@ -7,9 +11,36 @@ class User(db.Model, UserMixin):
     __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(40), nullable=False, unique=True)
     email = db.Column(db.String(255), nullable=False, unique=True)
+    full_name = db.Column(db.String(100), nullable=False)
+    username = db.Column(db.String(40), nullable=False, unique=True)
+    bio = db.Column(db.String(150))
+    profile_photo = db.Column(db.String(255))
     hashed_password = db.Column(db.String(255), nullable=False)
+    created_at = db.Column(DateTime(timezone=True), server_default=func.now())
+    updated_at = db.Column(DateTime(timezone=True), onupdate=func.now())
+
+    posts = db.relationship("Post", back_populates="user")
+    comments = db.relationship("Comment", back_populates="user")
+
+    user_likes = db.relationship("Post",
+            secondary=likes,
+            back_populates="post_likes",
+            cascade="all, delete"
+    )
+
+    followers = db.relationship(
+        "User",
+        secondary=follows,
+        primaryjoin=(follows.c.follower_id == id),
+        secondaryjoin=(follows.c.followed_id == id),
+        backref=db.backref("following", lazy="dynamic"),
+        lazy="dynamic"
+    )
+    # this relationship allows you to access both the collection of users
+    # that follow a given user (with user.followers), and the collection
+    # of users that a user follows (with user.following)
+
 
     @property
     def password(self):
@@ -25,6 +56,11 @@ class User(db.Model, UserMixin):
     def to_dict(self):
         return {
             'id': self.id,
+            'email': self.email,
+            'full_name': self.full_name,
             'username': self.username,
-            'email': self.email
+            'bio': self.bio,
+            'profile_photo': self.profile_photo,
+            'created_at': self.created_at,
+            'updated_at': self.updated_at
         }

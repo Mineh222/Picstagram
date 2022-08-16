e3ef0ee4833f4d0561efd20b3b06ea752dde9690
Mineh <minehgharabegi@gmail.com>
2022-08-16 12:40:14 -0700

    Refactoring code

--- react-app/src/components/Follows/index.js
+++ react-app/src/components/Follows/index.js
@@ -8,33 +8,17 @@ const Follows = ({profileUsername}) => {
 
     const sessionUser = useSelector((state) => state.session.user);
     const [follow, setFollow] = useState(false);
-    const [user, setUser] = useState();
-
-    // useEffect(() => {
-    //     for (let i = 0; i < sessionUser?.following.length; i++) {
-    //         let user = sessionUser.following[i]
-    //         if (user.username === profileUsername) {
-    //             setFollow(true)
-    //         } else {
-    //             setFollow(false)
-    //         }
-    //     }
-    // }, [sessionUser.following, profileUsername])
 
     useEffect(() => {
-        if (sessionUser?.following.find(user => user.username === profileUsername)) {
-            setFollow(true)
-        } else {
-            setFollow(false)
+        for (let i = 0; i < sessionUser.following.length; i++) {
+            let user = sessionUser.following[i]
+            if (user.username === profileUsername) {
+                setFollow(true)
+            }
         }
-
-        if (sessionUser?.username === profileUsername) {
-            setUser(true)
-        }
-    }, [sessionUser.following, sessionUser.username, profileUsername])
+    }, [sessionUser.following, profileUsername])
 
     const followButton = async (e) => {
-        e.preventDefault();
         setFollow(true)
         const follow = await dispatch(thunkFollow(profileUsername))
 
@@ -49,7 +33,6 @@ const Follows = ({profileUsername}) => {
     }
 
     const unfollowButton = async (e) => {
-        e.preventDefault();
         setFollow(false)
         const unfollow = await dispatch(thunkUnfollow(profileUsername))
 
@@ -64,15 +47,11 @@ const Follows = ({profileUsername}) => {
     }
 
     return (
-        !user ?
+        sessionUser.username !== profileUsername ?
             !follow ?
-                <form onSubmit={followButton}>
-                    <button id="follow-button">Follow</button>
-                </form>
+                    <button id="follow-button" onClick={followButton}>Follow</button>
                 :
-                <form onSubmit={unfollowButton}>
-                    <button id="unfollow-button">Unfollow</button>
-                </form>
+                    <button id="unfollow-button" onClick={unfollowButton}>Unfollow</button>
             :
             null
     )
